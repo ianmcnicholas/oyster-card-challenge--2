39e8cf8b79f5b82b033ef5aae96795a2b89e0ad2
cookie-mac <ianjmcnicholas@gmail.com>
2021-02-08 17:22:08 +0000

    Chapter 8 completed

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -9,11 +9,29 @@ class Oystercard
 
   def initialize(starting_balance = 0)
     @balance = starting_balance
+    @in_journey = false
   end
 
   def top_up(amount)
     fail "Maximum balance reached (£#{Max_Balance})" if (@balance + amount) > Max_Balance
     @balance += amount
+  end
+
+  def deduct(fare)
+    fail "You do not have enough money on your card for this fare" if (@balance - fare) < 0
+    @balance -= fare
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -7,12 +7,16 @@ describe Oystercard do
     expect(oystercard.balance).to eq 0
   end
 
+  it "is intially not in a journey" do
+    oystercard = Oystercard.new
+    expect(oystercard).not_to be_in_journey
+  end
+
   it "should respond to the method top_up with 1 arg" do
     expect(subject).to respond_to(:top_up).with(1).argument
   end
 
   describe "#top_up" do
-
     it "should top up the balance" do
       oystercard = Oystercard.new
       oystercard.top_up(1)
@@ -24,7 +28,52 @@ describe Oystercard do
     oystercard.top_up(Oystercard::Max_Balance)
     expect { oystercard.top_up(1) }.to raise_error "Maximum balance reached (£#{Oystercard::Max_Balance})"
     end
+  end
 
+  it "should respond deduct with 1 argument" do
+    expect(subject).to respond_to(:deduct).with(1).argument
+  end
+
+  describe "#fare" do
+    it "should reduce the balance" do
+      oystercard = Oystercard.new
+      oystercard.top_up(10)
+      oystercard.deduct(5)
+      expect(oystercard.balance).to eq 5
+    end
+
+    it "should throw an error if there is not enough money on the card to pay fare" do
+      oystercard = Oystercard.new
+      oystercard.top_up(1)
+      expect { oystercard.deduct(2) }.to raise_error "You do not have enough money on your card for this fare"
+    end
+  end
+
+  it "should respond to touch_in" do
+    expect(subject).to respond_to(:touch_in)
+  end
+
+  it "should respond to touch_out" do
+    expect(subject).to respond_to(:touch_out)
+  end
+
+  it "should respond to in_journey" do
+    expect(subject).to respond_to(:in_journey?)
+  end
+
+  describe "#in_journey" do
+    it "expects a user to be in a journey after they have touched in" do
+      oystercard = Oystercard.new
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+
+    it "expects a user to not be in a journey if they have touched out" do
+      oystercard = Oystercard.new
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
+    end
   end
 
 end
